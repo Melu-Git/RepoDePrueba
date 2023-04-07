7ae73401e509072113e323152e2f7b2b75ddeca4
Melina <violamelina.b@gmail.com>
2023-04-06 23:31:03 -0300

    segundo commit

--- src/ar/edu/unlam/PruebaGit/Colaboradores.java
+++ src/ar/edu/unlam/PruebaGit/Colaboradores.java
@@ -3,7 +3,7 @@ package ar.edu.unlam.PruebaGit;
 public class Colaboradores {
 
 	public static void main(String[] args) {
-		System.out.println("Hola!");
+		System.out.println("Hola! Como andan?");
 	}
 
 }
